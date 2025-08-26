98d710c9bc75b465234c522bccc682eefc4c50d5
池ノ上将基 <ikenojoushoumoto@ikenojoushoumotonoMacBook-Air.local>
2025-08-26 16:14:16 +0900

    新規作成機能実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -21,5 +21,16 @@ class TodoController extends Controller
       return view('todo.create', ['todos' => 'TODO:']); 
     }
 
+    public function store(Request $request)
+    {
+      $content = $request->input('content');
+
+      $todo = new Todo();
+      $todo->content = $content;
+      $todo->save();
+
+      return redirect()->route('todo.index');
+    }
+
 }
 
